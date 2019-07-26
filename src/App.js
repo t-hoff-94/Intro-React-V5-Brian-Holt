ee75fe762a432d3bb0c9d2c513c345b6f347a2d6
Tanner <tannerjhoffman@gmail.com>
2019-07-26 14:09:13 -0600

    end of hooks, start effects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Belle",
-      animal: "Dog",
-      breed: "Shitsoo"
-    }),
-    React.createElement(Pet, {
-      name: "Tucker",
-      animal: "Dog",
-      breed: "Shitsoo"
-    }),
-    React.createElement(Pet, {
-      name: "Tink",
-      animal: "Sugar Glider",
-      breed: "FlyingSquirrel"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", { class: "shit" }, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-    React.createElement("h2", {}, breed)
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{breed}</h2>
+      <h3>{animal}</h3>
+    </div>
+  );
 }

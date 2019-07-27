59d7a9ad55425cd7f7a93b05a672359a4eaa1369
Tanner <tannerjhoffman@gmail.com>
2019-07-27 16:09:23 -0600

    begin reach router section

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,48 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
+  console.log(this);
+
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
-  const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    console.log(animals);
+
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    console.log(pet);
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
-      <form action="submit">
+      <form
+        action="submit"
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -25,6 +57,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
--- src/useDropdown.js
+++ src/useDropdown.js
@@ -4,14 +4,14 @@ const useDropdown = (label, defaultState, options) => {
   const [state, setState] = useState(defaultState);
   const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
   const Dropdown = () => (
-    <label htmlFor="id">
+    <label htmlFor={id}>
       {label}
       <select
-        id="id"
+        id={id}
         value={state}
         onChange={e => setState(e.target.value)}
         onBlur={e => setState(e.target.value)}
-        disabled={options.length === 0}
+        disabled={!options.length}
       >
         <option>All</option>
         {options.map(item => (
